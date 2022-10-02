d94e131c2c732004bc4b81261899c4a596b50e4a
sethiyasanskar63 <101315790+sethiyasanskar63@users.noreply.github.com>
2022-10-02 19:50:33 +0530

    Add files via upload

---
+++ Sanskar_FOPJava_GradedProject1/src/com/gl/gradedassessment/HrDepartment.java
@@ -0,0 +1,24 @@
+package com.gl.gradedassessment;
+
+public class HrDepartment extends SuperDepartment {
+
+	@Override
+	public String departmentName() {
+		return "HR Department";
+	}
+
+	@Override
+	public String getTodaysWork() {
+		return "Fill today’s timesheet and mark your attendance";
+	}
+
+	@Override
+	public String getWorkDeadline() {
+		return "Complete by EOD";
+	}
+
+	public String doActivity() {
+		return "team Lunch";
+	}
+
+}
